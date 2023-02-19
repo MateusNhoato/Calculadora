f1daa56e419e2f3f8b7f18b70bee9ede57394f48
Mateus Nhoato <manhoato@gmail.com>
2023-02-19 14:35:15 -0300

    Adicionando descanso ao pomodoro

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,36 +1,54 @@
 const botaoStart = document.getElementById("botao-start");
 const botaoReset = document.getElementById("botao-reset");
 const timer = document.getElementById("timer");
+const modo = document.getElementById("modo");
+const numeroSessoes = document.getElementById("sessoes");
 
-botaoStart.addEventListener("click", function () {
-  if (timer.innerText === "00:00") {
-    tocarSom("audio/botao.mp3");
+function pomodoro(){
     let segundos = 0;
-    let interval = setInterval(function () {
-      segundos += 1;
-      let minutos = Math.floor(segundos / 60);
-      minutosDisplay = minutos;
-      segundosDisplay = segundos % 60;
+    let tempo = 25;
 
-      if (segundosDisplay < 10) segundosDisplay = `0${segundosDisplay}`;
+    setInterval(function () {
+    segundos += 1;
+    let minutos = Math.floor(segundos / 60);
+    minutosDisplay = minutos;
+    segundosDisplay = segundos % 60;
 
-      if (minutosDisplay < 10) minutosDisplay = `0${minutos}`;
+    if (segundosDisplay < 10) segundosDisplay = `0${segundosDisplay}`;
+    if (minutosDisplay < 10) minutosDisplay = `0${minutos}`;
+    
+    console.log(`${minutosDisplay}:${segundosDisplay}`)
+    timer.innerText = `${minutosDisplay}:${segundosDisplay}`;
 
-      timer.innerText = `${minutosDisplay}:${segundosDisplay}`;
-
-      if (minutos >= 25) {
-        clearInterval(interval);
-        tocarSom("audio/alarme.mp3");
-        timer.innerText = "00:00";
+    if (minutos >= tempo) {  
+      tocarSom("audio/alarme.mp3");
+      if(tempo === 25)
+      {
+        tempo = 5;
+        modo.innerText = "Descanso";
       }
-    }, 1000);
-  }
-});
-botaoReset.addEventListener("click", function () {
-  location.reload();
-});
+      else
+       {
+        tempo = 25;
+        modo.innerText = "Trabalho";
+       }
+      segundos = 0;
+      timer.innerText = "00:00";
+    }
+  }, 1000);
+}
 
 function tocarSom(som) {
   let audio = new Audio(som);
   audio.play();
 }
+
+botaoStart.addEventListener("click",function () {
+  if (timer.innerText === "00:00") {
+    tocarSom("audio/botao.mp3");
+    pomodoro();
+}
+});
+botaoReset.addEventListener("click", function () {
+  location.reload();
+});
